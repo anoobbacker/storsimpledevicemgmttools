aaae7442c3f1953cf0b1d015f01c98c822f4fcc2
Anoob Backer <anoobbacker@users.noreply.github.com>
2019-07-24 15:31:57 +0530

    Update Monitor-Backups.ps1

--- runbooks/Monitor-Backups.ps1
+++ runbooks/Monitor-Backups.ps1
@@ -81,8 +81,8 @@
             New-AzureRmAutomationCredential -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name "Mail-Credential" -Value $Creds
             
     10. To ensure that the automation account created can access the StorSimple Device Manager service, you need to assign appropriate 
-        permissions to the automation account. Go to Access control in your StorSimple Device Manager service. 
-        Click + Add and provide the name of your Azure Automation Account. Save the settings.
+        permissions to the automation account. Go to Access control in your StorSimple Device Manager service, add Reader or higher privileged 
+        roles. 
         Refer https://docs.microsoft.com/azure/storsimple/storsimple-8000-automation-azurerm-runbook
 
      ----------------------------
